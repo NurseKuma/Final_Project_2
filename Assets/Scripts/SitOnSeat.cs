e25bb2ad1ddbb8b5dd06af8be373192621f2c818
NurseKuma <narapat.moun@bumail.net>
2025-12-01 22:06:10 +0700

    last

---
+++ Assets/Scripts/SitOnSeat.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class SitOnSeat : MonoBehaviour
+{
+    [Header("Targets")]
+    public Transform seatPoint;
+    public Transform playerRig;
+
+    [Header("Buttons")]
+    public OVRInput.Button enterButton = OVRInput.Button.One;
+    public OVRInput.Button exitButton = OVRInput.Button.Two;
+
+    private bool canEnter = false;
+    private bool isSitting = false;
+    private Transform trackingSpace;
+
+    void Start()
+    {
+        trackingSpace = playerRig.Find("TrackingSpace");
+        if (trackingSpace == null)
+            Debug.LogError("TrackingSpace ไม่พบ!");
+    }
+
+    void Update()
+    {
+        if (canEnter && !isSitting && OVRInput.GetDown(enterButton))
+            Sit();
+
+        if (isSitting && OVRInput.GetDown(exitButton))
+            ExitSeat();
+    }
+
+    void Sit()
+    {
+        isSitting = true;
+
+        // Parent TrackingSpace ไปยัง SeatPoint
+        trackingSpace.SetParent(seatPoint);
+        trackingSpace.localPosition = Vector3.zero;
+        trackingSpace.localRotation = Quaternion.identity;
+    }
+
+    void ExitSeat()
+    {
+        isSitting = false;
+
+        // ปล่อย parent
+        trackingSpace.SetParent(null);
+
+       
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            canEnter = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            canEnter = false;
+    }
+}
